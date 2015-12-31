c27039d1c28a32fd8f3adf76217a3bf462f2e659
Jorge Enrique Gamboa Fuentes <jegamboafuentes@gmail.com>
2015-12-31 04:10:32 -0500

    New Examples module, New module in main page, with some web pages examples

--- New York Factory Systems/js/index.js
+++ New York Factory Systems/js/index.js
@@ -1,3 +1,37 @@
-//$(window).load(function(){
+//Under construction script
+////$(window).load(function(){
 //    $('#underConstructionModal').modal('show');
 //});
+
+//<!-- Custom Theme JavaScript -->
+// Closes the sidebar menu
+$("#menu-close").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            if ((this.hash) != "#myCarousel") {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            } else {
+                
+            }
+        }
+    });
+});
